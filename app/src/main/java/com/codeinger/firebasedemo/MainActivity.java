a9464efc59ed46330cef26f8adcbb52a590de62c
codeinger <codeingercode@gmail.com>
2019-08-26 11:31:28 +0530

    data inserted

---
+++ app/src/main/java/com/codeinger/firebasedemo/MainActivity.java
@@ -0,0 +1,69 @@
+package com.codeinger.firebasedemo;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText title,description,author;
+    private Button save;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        title = findViewById(R.id.title);
+        description = findViewById(R.id.description);
+        author = findViewById(R.id.author);
+        save = findViewById(R.id.save);
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                HashMap<String,Object> map = new HashMap<>();
+                map.put("title",title.getText().toString());
+                map.put("description",description.getText().toString());
+                map.put("author",author.getText().toString());
+
+                FirebaseDatabase.getInstance().getReference().child("Post").push()
+                        .setValue(map)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                Log.i("jfbvkj", "onComplete: ");
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.i("jfbvkj", "onFailure: "+e.toString());
+                    }
+                }).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.i("jfbvkj", "onSuccess: ");
+                    }
+                });
+
+
+            }
+        });
+
+    }
+}
--- app/src/test/java/com/codeinger/firebasedemo/ExampleUnitTest.java
+++ app/src/test/java/com/codeinger/firebasedemo/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.codeinger.connectfirebasewithandroid;
+package com.codeinger.firebasedemo;
 
 import org.junit.Test;
 
